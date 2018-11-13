a27294e06f43c866ddf4712b86d7a8f4d4b412c6
est31 <MTest31@outlook.com>
2018-11-13 02:57:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## glium-brush
+
+[Glium](https://github.com/glium/glium) frontend for the [glyph-brush](https://github.com/alexheretic/glyph-brush) text renderer.
+
+It bases on the gfx-brush crate. While it "works" right now, it's quite WIP atm as some parts of the API are only drafted.
+
+TODO:
+
+* add builder struct like the glyph-brush crate has
+* complete code style changes
+* examples and docs are sitll about the gfx-brush crate
---
+++ examples/hello.rs
@@ -0,0 +1,61 @@
+extern crate glium;
+extern crate glium_brush;
+
+use glium::{glutin, Surface};
+use glium::glutin::{Api, GlProfile, GlRequest};
+
+use glium_brush::GlyphBrush;
+use glium_brush::glyph_brush::{
+	rusttype::Font, Section,
+};
+
+pub fn main() {
+	let mut events_loop = glutin::EventsLoop::new();
+	let window = glutin::WindowBuilder::new();
+	let context = glutin::ContextBuilder::new().with_gl_profile(GlProfile::Core)
+			.with_gl(GlRequest::Specific(Api::OpenGl, (3, 2)))
+			.with_srgb(true);
+	let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+	let dejavu: &[u8] = include_bytes!("../fonts/DejaVuSans-2.37.ttf");
+	let fonts = vec![Font::from_bytes(dejavu).unwrap()];
+
+	let mut glyph_brush = GlyphBrush::new(&display, fonts);
+
+	loop {
+		let screen_dims = display.get_framebuffer_dimensions();
+
+		glyph_brush.queue(Section {
+			text : "Hello, World!",
+			bounds : (screen_dims.0 as f32, screen_dims.1 as f32 / 2.0),
+			..Section::default()
+		});
+		glyph_brush.queue(Section {
+			text : "This is in the middle of the screen",
+			bounds : (screen_dims.0 as f32, screen_dims.1 as f32 / 2.0),
+			screen_position : (0.0, screen_dims.1 as f32 / 2.0),
+			scale : glyph_brush::rusttype::Scale::uniform(16.0),
+			..Section::default()
+		});
+
+		glyph_brush.draw_queued(&display, || {
+			let mut target = display.draw();
+			target.clear_color_and_depth((1.0, 1.0, 1.0, 0.0), 1.0);
+			target
+		});
+
+		let mut exit = false;
+		events_loop.poll_events(|event| {
+			match event {
+				glutin::Event::WindowEvent { event, .. } => match event {
+					glutin::WindowEvent::CloseRequested => exit = true,
+					_ => (),
+				},
+				_ => (),
+			}
+		});
+		if exit {
+			break;
+		}
+	}
+}
